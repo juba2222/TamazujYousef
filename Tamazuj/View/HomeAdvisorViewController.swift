6551e52a8ee6f6ed62f281ced25e4c9c09b4cafe
yousef omar <zeef059@gmail.com>
2019-09-14 19:03:06 +0300

    category page

---
+++ Tamazuj/View/HomeAdvisorViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeAdvisorViewController.swift
+//  Tamazuj
+//
+//  Created by Saadi on 9/6/19.
+//  Copyright © 2019 Tamazuj. All rights reserved.
+//
+
+import UIKit
+
+class HomeAdvisorViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.separatorStyle = .none
+    }
+}
+
+extension HomeAdvisorViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
